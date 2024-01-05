b292550644b6e2d888b41307e818242321d6daae
joeposthauer <posthauerjoe@gmail.com>
2024-01-05 15:46:27 -0800

    added stuff

--- mysite/PlaylistGenerator/credentials.py
+++ mysite/PlaylistGenerator/credentials.py
@@ -1,7 +1,7 @@
 # all authentication credentials to access Spotify API
 client_ID='eb135b77a46b4ea1888a268eedb8880a'       
 client_SECRET='27a275c9e3bd4ffa9782f972ed0b3ccf'   
-redirect_URI="http://127.0.0.1:8000/PlaylistGenerator/"  
+redirect_URI="http://127.0.0.1:8000/PlaylistGenerator/parameters/"  
 username=''                                                                                                   
 scope = "user-read-private playlist-read-private playlist-modify-public user-top-read user-read-email user-library-read"  
 
