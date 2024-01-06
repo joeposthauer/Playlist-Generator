50e0cd3aefad8a1ae69a81635e881e87b4133395
gillianfeder <gfeder510@gmail.com>
2024-01-05 18:12:51 -0700

    fixing auth redirect

--- mysite/PlaylistGenerator/views.py
+++ mysite/PlaylistGenerator/views.py
@@ -9,7 +9,7 @@ from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 
 # View for when a user enters site- needs to authenticate
 def index(request):
-    
+    args = {}
     # Create a SpotifyOAuth object
     sp_oauth = SpotifyOAuth(client_id=credentials.client_ID, client_secret=credentials.client_SECRET, redirect_uri=credentials.redirect_URI, scope = credentials.scope)
 
@@ -21,9 +21,13 @@ def index(request):
     url = sp_oauth.get_authorize_url()
     # Print the authorization url to the console
     print(url)
+    args['url'] = url
 
+    access_token = request.session.get("access_token")
+    print('\n\n ACCESS TOKEN: ', access_token, '\n\n')
+    
     # Redirect the user to the Spotify login page
-    return HttpResponseRedirect(url)
+    return render(request, "landing.html", args)
 
 # View for when a user authenticates- choosing parameters
 def parameters(request):
