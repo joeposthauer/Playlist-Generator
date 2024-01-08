69ab6b4f499294f568002c4708006c36611d7fac
gillianfeder <gfeder510@gmail.com>
2024-01-07 20:36:35 -0700

    can get params

--- mysite/PlaylistGenerator/views.py
+++ mysite/PlaylistGenerator/views.py
@@ -67,26 +67,32 @@ def parameters(request):
         print("The access token is invalid or has expired.\n\n")
 
     # if this is a POST request we need to process the form data
-    if request.method == "POST":
-        # create a form instance and populate it with data from the request:
-        form = ParamsForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            url = reverse("/calculate/", form={'data': form})
-            return HttpResponseRedirect(url)
+    print("right before post conditional")
+    
 
     # if a GET (or any other method) we'll create a blank form
-    else:
-        form = ParamsForm()
+   
+    form = ParamsForm()                              #only ever enters this case
 
     args['form'] = form
     return render(request, "parameters.html", args)
 
 # View for calculating parameters for playlist
 def calculate(request, *args):
+
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = ParamsForm(request.POST)
+        # check whether it's valid:
+        print("right before valid conditional")
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            print(form.cleaned_data)
+            print(form.cleaned_data['genres'])
+            # url = reverse("/calculate/", form={'data': form})
+            return HttpResponse('thanks')
 
     print(args)
     return render(request, "calculate.html")
